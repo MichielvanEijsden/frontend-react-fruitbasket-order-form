8efefd6367d7724b6c061c453090445cdb7bbb8b
MichielvanEijsden <143597805+MichielvanEijsden@users.noreply.github.com>
2023-12-02 17:00:12 +0100

    contact form gemaakt no slyling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,100 @@
 import './App.css'
 
 
-import Products from "./helpers/Products.jsx";
-
+import Fruits from "./components/fruits/Fruits.jsx";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 
 function App() {
 
-return(
-    <>
-    <div>
-        <Products/>
-    </div>
-    </>
-)
+    function handleFormSubmit(data) {
+        console.log(data)
+    }
+
+    const [aardbeiCount, setAardbeiCount] = useState(0)
+    const [mangoCount, setMangoCount] = useState(0)
+    const [kiwiCount, setKiwiCount] = useState(0)
+    const [peerCount, setPeerCount] = useState(0)
+    const {register, handleSubmit} = useForm()
+
+    return (
+        <>
+            <h1>Fruitmand bezorgservice</h1>
+
+            <Fruits
+                fruitType="🍓 Aardbei"
+                count={aardbeiCount}
+                setCount={setAardbeiCount}
+            />
+            <Fruits
+                fruitType="🥭 Mango"
+                count={mangoCount}
+                setCount={setMangoCount}
+            />
+            <Fruits
+                fruitType="🥝 Kiwi"
+                count={kiwiCount}
+                setCount={setKiwiCount}
+            />
+            <Fruits
+                fruitType="🍐 Peer"
+                count={peerCount}
+                setCount={setPeerCount}
+            />
+
+            <section className='schoppingCard'>
+                <button type="button"
+                        onClick={() => setAardbeiCount(0) || setMangoCount(0) || setPeerCount(0) || setKiwiCount(0)}>Reset
+                </button>
+            </section>
 
 
+            <form id="contactForm" onSubmit={handleSubmit(handleFormSubmit)} className="contactForm">
 
+                <label>
+                    Voornaam:
+                    <input type="text" {...register("firstName")}/>
+                </label>
+                <label>
+                    Achternaam:
+                    <input type="text"{...register("lastName")}/>
+                </label>
+                <label>
+                    Leeftijd:
+                    <input type="number"{...register("age")}/>
+                </label>
+                <label>
+                    Postcode:
+                    <input type="text" {...register("zipCode")}/>
+                </label>
+                <label>
+                    Bezorgfrequentie:
+                    <select {...register("delivery")}>
+                        <option value="iedere_week">iedere week</option>
+                        <option value="om_de_week">om de week</option>
+                        <option value="iedere_maand">iedere maand</option>
+                    </select>
+                </label>
+                <label>
+                    <input type="radio" name="day" {...register("delivery-time")} placeholder="day"/>Overdag
+                    <input type="radio" name="evening" {...register("delivery-time")} placeholder="evening"/>s Avonds
+                </label>
+                <label>
+                    Opmerkingen:
+                    <textarea rows="4" cols="40" {...register("comments")}/>
+                </label>
+                <label>
+                    <input type="checkbox" placeholder="terms_of_agreement"/>Ik ga akkoord met de voorwaarden
+                </label>
+                <button type="submit">
+                    Verzend
+                </button>
+            </form>
+
+
+        </>
+    )
 }
 
 
--- src/components/fruits/Fruits.jsx
+++ src/components/fruits/Fruits.jsx
@@ -1,12 +1,12 @@
 import './Fruits.css'
 
-function Fruits(prop){
 
+function Fruits(prop){
     return(
-        <section className='schoppingCard'>
-        <h2>{prop.fruitType}</h2>
+        <section className='schoppingCard' >
+            <h2>{prop.fruitType}</h2>
             <button type='button' disabled={prop.count === 0} onClick={() => prop.setCount(prop.count -1)}>-</button>
-        <p>{prop.count}</p>
+            <p>{prop.count}</p>
             <button type='button' onClick={() => prop.setCount(prop.count +1)}>+</button>
         </section>
 
