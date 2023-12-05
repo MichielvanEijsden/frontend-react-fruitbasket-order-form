d9bc8a9d1e4263df1eb12d550f02bd7392c9df56
MichielvanEijsden <143597805+MichielvanEijsden@users.noreply.github.com>
2023-12-05 13:08:37 +0100

    reset button aangepast na nakijken

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Fruits from "./components/fruits/Fruits.jsx";
 import {useState} from "react";
 import {useForm} from "react-hook-form";
 
+
 function App() {
 
     const [aardbeiCount, setAardbeiCount] = useState(0)
@@ -11,6 +12,13 @@ function App() {
     const [kiwiCount, setKiwiCount] = useState(0)
     const [peerCount, setPeerCount] = useState(0)
     const {register, handleSubmit} = useForm()
+    function resetFruits(){
+        setAardbeiCount(0)
+        setMangoCount(0)
+        setKiwiCount(0)
+        setPeerCount(0)
+    }
+
     function handleFormSubmit(data) {
        const aardbei = `Aardbeien: ${aardbeiCount}`
         const mango = `Mangoes: ${mangoCount}`
@@ -44,49 +52,52 @@ function App() {
                     setCount={setPeerCount}
                 />
                 <section className='resetButton'>
-                    <button type="button" onClick={() => setAardbeiCount(0) || setMangoCount(0) || setPeerCount(0) || setKiwiCount(0)}>Reset</button>
+                    <button type="button" onClick={() => {resetFruits()}}>Reset</button>
                 </section>
-            <form  onSubmit={handleSubmit(handleFormSubmit)} className="contactForm">
-                <label>
-                    Voornaam:
-                    <input type="text" {...register("firstName",{required:{value: true,message:'vul je voornaam in'}})}/>
-                </label>
-                <label>
-                    Achternaam:
-                    <input type="text"{...register("lastName",{required:{value: true,message:'vul je achternaam in'}})}/>
-                </label>
-                <label>
-                    Leeftijd:
-                    <input type="number"{...register("age",{required:{value: true,message:'vul je leeftijd in'}})}/>
-                </label>
-                <label>
-                    Postcode:
-                    <input type="text" {...register("zipCode",{required:{value: true,message:'vul je postcode in'}})}/>
-                </label>
-                <label>
-                    Bezorgfrequentie:
-                    <select {...register("delivery")}>
-                        <option value="iedere_week">iedere week</option>
-                        <option value="om_de_week">om de week</option>
-                        <option value="iedere_maand">iedere maand</option>
-                    </select>
-                </label>
-                <label className='list'>
-                    <input type="radio" name="day" {...register("delivery-time")} placeholder="day"/>Overdag
-                    <input type="radio" name="evening" {...register("delivery-time")} placeholder="evening"/>s Avonds
-                </label>
-                <label className='list'>
-                    Opmerkingen:
-                    <textarea rows="4" cols="40" {...register("comments",{maxLength:{value: 100,message:'het bericht is te lang'}})}/>
-                </label>
-                <label>
-                    <input type="checkbox" {...register("terms_of_agreement",{required:{value: true,message:'je moet deze nog aan vinken'}})} placeholder="terms_of_agreement"/>Ik ga akkoord met de voorwaarden
-                </label>
 
-                <button type="submit" >
-                    Verzend
-                </button>
-            </form>
+                <form  onSubmit={handleSubmit(handleFormSubmit)} className="contactForm">
+                    <label>
+                        Voornaam:
+                        <input type="text" {...register("firstName",{required:{value: true,message:'vul je voornaam in'}})}/>
+                    </label>
+                    <label>
+                        Achternaam:
+                        <input type="text"{...register("lastName",{required:{value: true,message:'vul je achternaam in'}})}/>
+                    </label>
+                    <label>
+                        Leeftijd:
+                        <input type="number"{...register("age",{required:{value: true,message:'vul je leeftijd in'}})}/>
+                    </label>
+                    <label>
+                        Postcode:
+                        <input type="text" {...register("zipCode",{required:{value: true,message:'vul je postcode in'}})}/>
+                    </label>
+                    <label>
+                        Bezorgfrequentie:
+                        <select {...register("delivery")}>
+                            <option value="iedere_week">iedere week</option>
+                            <option value="om_de_week">om de week</option>
+                            <option value="iedere_maand">iedere maand</option>
+                        </select>
+                    </label>
+                    <label className='list'>
+                        <input type="radio" name="day" {...register("delivery-time")} placeholder="day"/>Overdag
+                        <input type="radio" name="evening" {...register("delivery-time")} placeholder="evening"/>s Avonds
+                    </label>
+                    <label className='list'>
+                        Opmerkingen:
+                        <textarea rows="4" cols="40" {...register("comments",{maxLength:{value: 100,message:'het bericht is te lang'}})}/>
+                    </label>
+                    <label>
+                        <input type="checkbox" {...register("terms_of_agreement",{required:{value: true,message:'je moet deze nog aan vinken'}})} placeholder="terms_of_agreement"/>Ik ga akkoord met de voorwaarden
+                    </label>
+
+                    <button type="submit" >
+                        Verzend
+                    </button>
+                </form>
+
+
             </div>
             </div>
         </>
--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -7,22 +7,22 @@ function ContactForm() {
     }
     const {register, handleSubmit} = useForm()
     return (
-        <form id="contactForm" onSubmit={handleSubmit(handleFormSubmit)} className="contactForm">
+        <form  onSubmit={handleSubmit(handleFormSubmit)} className="contactForm">
             <label>
                 Voornaam:
-                <input type="text" {...register("firstName")}/>
+                <input type="text" {...register("firstName",{required:{value: true,message:'vul je voornaam in'}})}/>
             </label>
             <label>
                 Achternaam:
-                <input type="text"{...register("lastName")}/>
+                <input type="text"{...register("lastName",{required:{value: true,message:'vul je achternaam in'}})}/>
             </label>
             <label>
                 Leeftijd:
-                <input type="number"{...register("age")}/>
+                <input type="number"{...register("age",{required:{value: true,message:'vul je leeftijd in'}})}/>
             </label>
             <label>
                 Postcode:
-                <input type="text" {...register("zipCode")}/>
+                <input type="text" {...register("zipCode",{required:{value: true,message:'vul je postcode in'}})}/>
             </label>
             <label>
                 Bezorgfrequentie:
@@ -32,21 +32,18 @@ function ContactForm() {
                     <option value="iedere_maand">iedere maand</option>
                 </select>
             </label>
-            <label>
+            <label className='list'>
                 <input type="radio" name="day" {...register("delivery-time")} placeholder="day"/>Overdag
                 <input type="radio" name="evening" {...register("delivery-time")} placeholder="evening"/>s Avonds
             </label>
-            <label>
+            <label className='list'>
                 Opmerkingen:
-                <textarea rows="4" cols="40" {...register("comments")}/>
+                <textarea rows="4" cols="40" {...register("comments",{maxLength:{value: 100,message:'het bericht is te lang'}})}/>
             </label>
             <label>
-                <input type="checkbox" placeholder="terms_of_agreement"/>Ik ga akkoord met de voorwaarden
+                <input type="checkbox" {...register("terms_of_agreement",{required:{value: true,message:'je moet deze nog aan vinken'}})} placeholder="terms_of_agreement"/>Ik ga akkoord met de voorwaarden
             </label>
-            <label
-                {...register("Aardbei")}>
-                {}
-            </label>
+
             <button type="submit" >
                 Verzend
             </button>
